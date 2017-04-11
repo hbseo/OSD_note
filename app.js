139148f3a049f2b60e9555a3a145ddfc78111012
alchon <gusqja8753@gmail.com>
2017-04-11 11:37:51 +0900

    .

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// MongoDB
+mongoose.connect('mongodb://alchon:anjfqhk123@ds157740.mlab.com:57740/osd_note');
+
+var Schema = mongoose.Schema;
+
+var MemoSchema = new Schema({
+  title : String,
+  contents : String,
+  date : {type:Date, default:Date.now}
+});
+
+var Memo = mongoose.model('Memo', MemoSchema);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +32,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('bower_components'));
@@ -43,5 +57,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.post('/new', (req,res) => {
+  var newMemo = new Memo();
+  newMemo.title = req.body.title;
+  newMemo.contents = req.body.contents;
+  newMemo.save(function(err) {
+    if(err) console.log("Something wrong");
+    else res.redirect('/');
+  })
+})
 
 module.exports = app;
