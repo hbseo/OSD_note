0a6c7f63dda8127b124897f3fc76afd2b3656770
alchon <gusqja8753@gmail.com>
2017-04-11 14:44:55 +0900

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# OSD_note
--- app.js
+++ app.js
@@ -1,71 +1,34 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var bodyParser = require('body-parser');
+var autoIncrement = require("mongoose-auto-increment");
 var app = express();
 
-// MongoDB
 mongoose.connect('mongodb://alchon:anjfqhk123@ds157740.mlab.com:57740/osd_note');
+
+var connection = mongoose.connection;
+
+autoIncrement.initialize(connection)
 
 var Schema = mongoose.Schema;
 
 var MemoSchema = new Schema({
-  title : String,
-  contents : String,
-  date : {type:Date, default:Date.now}
+    title     : String,
+    body      : String,
+    date      : {type: Date, default:Date.now}
 });
 
+MemoSchema.plugin(autoIncrement.plugin, 'Memo');
 var Memo = mongoose.model('Memo', MemoSchema);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('bower_components'));
+app.set('view engine', 'pug');
+app.set('views', 'view');
 
-app.use('/', index);
-app.use('/users', users);
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+
+app.listen('3000', () => {
+    console.log("Express is Connected 3000 Port!");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.post('/new', (req,res) => {
-  var newMemo = new Memo();
-  newMemo.title = req.body.title;
-  newMemo.contents = req.body.contents;
-  newMemo.save(function(err) {
-    if(err) console.log("Something wrong");
-    else res.redirect('/');
-  })
-})
-
-module.exports = app;
