ee9891d390e73c4f2ba6bf46451b9f18ac738543
alchon <gusqja8753@gmail.com>
2017-04-14 00:22:52 +0900

    test weather

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var autoIncrement = require("mongoose-auto-increment");
+var request = require('request');
+var cheerio = require('cheerio')
 var app = express();
+
+
 
 mongoose.connect('mongodb://alchon:anjfqhk123@ds157740.mlab.com:57740/osd_note');
 
@@ -54,8 +58,33 @@ app.get('/schedule', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    var url = 'https://www.accuweather.com/ko/kr/ansan/223641/weather-forecast/223641'
+    request(url, function(err, res, html) {  
+        if(!err) {
+            var $ = cheerio.load(html);
+
+            
+
+            var data = $('.night current first cl').children().children('.info').children('.temp');
+            var temp = $(data).find('.large-temp').text();
+            var realfeel = $(data).find('.realfeel').text();
+            var cond = $(data).find('.cond').text();
+            console.log(temp)
+            console.log(realfeel)
+            console.log(cond)
+            console.log("asdfasdfasdf")
+        }
+    });
     res.render('weather');
 })
+
+var feed       = document.getElementById('feed-tabs')
+var ul         = feed.getElementsByTagName('ul')[0];
+var li         = ul.getElementsByTagName('li')[0];
+var large_temp = li.getElementsByClassName('large-temp')[0];
+var temp_label = li.getElementsByClassName('temp-label')[0];
+var realfeel   = li.getElementsByClassName('realfeel')[0];
+var cond       = li.getElementsByClassName('cond')[0];
 
 app.post('/new', (req, res) => {
     var newMemo = new Memo();
@@ -66,6 +95,8 @@ app.post('/new', (req, res) => {
       res.redirect('/note');
     });
 });
+
+
 
 app.get('/memo/:id', (req, res) => {
 
